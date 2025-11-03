4f7de6e3707dd6150b045b882a1db57f9244b833
MarcusSerrano <161587718+MarcusSerrano@users.noreply.github.com>
2025-11-03 00:54:29 -0800

    Update config.h

--- ee470/assignment3/assignment3API/include/config.h
+++ ee470/assignment3/assignment3API/include/config.h
@@ -2,11 +2,11 @@
 
 // Wi-Fi settings
 // Home PC
-//const char* WIFI_SSID     = "DESKTOP-38G0AHI 4388";
-//const char* WIFI_PASSWORD = "/13x12L3";
+//const char* WIFI_SSID     = "";
+//const char* WIFI_PASSWORD = "";
 // Laptop
-const char* WIFI_SSID     = "DESKTOP-16CTLBL";
-const char* WIFI_PASSWORD = "1?8e658Y";
+const char* WIFI_SSID     = "";
+const char* WIFI_PASSWORD = "";
 
 // Remote file URL
 const char* URL_RESULTS   = "https://marcusinternetofthings.com/results.txt";
@@ -30,3 +30,4 @@ const bool RGB_ACTIVE_LOW = false;  // set false if your strip/LED is active-hig
 
 // JSON results file from the webpage sliders
 const char* URL_RESULTS_RGB = "https://marcusinternetofthings.com/results2.txt";
+
